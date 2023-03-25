11f8bbeff91f2d55a027654774a5faf94a8c1028
han5010 <error404@live.unc.edu>
2023-03-24 23:43:13 -0400

    Put fetched profile data into a more readable table format

---
+++ app/components/profiletable.jsx
@@ -0,0 +1,64 @@
+'use client'
+import { useMemo } from 'react'
+import MaterialReactTable from "material-react-table";
+
+import '../../styles/profiletable.css'
+
+const Profiletable = ( {profiles} ) => {
+    
+    // 원래 받던 데이타에서 오리엔테이션 필드가 boolean 인데, string 으로 바꿔야 테이블에 나타난다
+    const profiles_oriented = profiles.data.map(obj => {
+      return {...obj, orientation: obj.orientation.toString()} 
+    })
+
+    const columns = useMemo( // 제3자 라이브러리가 필요한 데이타
+      () => [
+        {
+          accessorKey: 'first_name', 
+          header: 'First Name',
+        },
+        {
+          accessorKey: 'last_name',
+          header: 'Last Name',
+        },
+        {
+          accessorKey: 'email', 
+          header: 'Email',
+        },
+        {
+          accessorKey: 'phone',
+          header: 'Phone',
+        },
+        {
+          accessorKey: 'city',
+          header: 'City',
+        },
+        {
+          accessorKey: 'state',
+          header: 'State',
+        },
+        {
+          accessorKey: 'address',
+          header: 'Address',
+        },
+        {
+          accessorKey: 'zip',
+          header: 'Zip'
+        },
+        {
+          accessorKey: 'orientation',
+          header: 'Oriented',
+        }
+      ],
+      [],
+    );
+
+    return (
+        <div>
+          <h2>Profiles Table</h2>
+          <MaterialReactTable columns={columns} data={profiles_oriented} />;
+        </div>
+    )
+}
+
+export default Profiletable
---
+++ app/components/reroute.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import Head from "next/head"
+
+import "../../styles/reroute.css"
+
+const Reroute = () => {
+    return (
+        <div>
+            <Head>
+                <title>MySmile</title>
+            </Head>
+            <div className="reroute-container">  {/* for some reason, calling this "container" calls the "container" class in login.css */}
+                
+                <div className="reroute-messagebox">
+                    <p>이용자님은 계정은 있는데, 관리자가 않이라서 이 어플을 이용할수 없습니다.</p>
+                    <p>오류가 있었으면 웨이크 스마일 치과 의원 관리자에게 연락해 주세요.</p>
+                    <div className="reroute-goback">
+                        <Link href="/" style={{ textDecoration: 'none' }}>고백</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Reroute
