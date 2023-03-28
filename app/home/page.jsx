de43d7bcf28cae0797868a9dc5237b684bd97c02
yuualice <58484241+yuualice@users.noreply.github.com>
2023-03-27 22:12:26 -0400

    Added tailwind and daisy UI dependencies

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,135 +1,142 @@
-'use client'
-import { supabase } from '../../supabaseClient'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import { supabase } from "../../supabaseClient";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-import Reroute from '../components/reroute'
-import Loading from '../loading'
-import Profiletable from '../components/profiletable'
-import Signuptable from '../components/signuptable'
-import Shiftstable from '../components/shiftstable'
-
-import "../../styles/homepage.css"
+import Reroute from "../components/reroute";
+import Loading from "../loading";
+import Profiletable from "../components/profiletable";
+import Signuptable from "../components/signuptable";
+import Shiftstable from "../components/shiftstable";
+import "../../styles/homepage.css";
 
 export default function Home() {
-    const [user, setUser] = useState(null)
-    const [people, setPeople] = useState(null)  
-    const [signups, setSignups] = useState(null)
-    const [shifts, setShifts] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [content, setContent] = useState("None")
-    const router = useRouter()
+  const [user, setUser] = useState(null);
+  const [people, setPeople] = useState(null);
+  const [signups, setSignups] = useState(null);
+  const [shifts, setShifts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [content, setContent] = useState("None");
+  const router = useRouter();
 
-    const fetchProfiles = async () => {
-      await supabase
+  const fetchProfiles = async () => {
+    await supabase
       .from("profiles")
       .select()
       .then((profiles, err) => {
         if (profiles) {
-          setPeople(profiles)
+          setPeople(profiles);
         } else {
-          console.log("error in profiles")
+          console.log("error in profiles");
         }
       })
-      .then(console.log("this is profiles"))
-    }
+      .then(console.log("this is profiles"));
+  };
 
-    const fetchSignups = async () => {
-      await supabase
+  const fetchSignups = async () => {
+    await supabase
       .from("signups")
       .select()
       .then((signups, err) => {
         if (signups) {
-          setSignups(signups)
+          setSignups(signups);
         } else {
-          console.log("error in signups")
+          console.log("error in signups");
         }
       })
-      .then(console.log("this is signups"))
-    }
+      .then(console.log("this is signups"));
+  };
 
-    const fetchShifts = async () => {
-      await supabase
+  const fetchShifts = async () => {
+    await supabase
       .from("shifts")
       .select()
       .then((shifts, err) => {
         if (shifts) {
-          setShifts(shifts)
+          setShifts(shifts);
         } else {
-          console.log("error in shifts")
+          console.log("error in shifts");
         }
       })
-      .then(console.log("this is shifts"))
-    }
-  
-    useEffect(() => {  // 모든 테이블 정보 한 번에 얻기
-      const fetchTables = async() => {
-        try {
-          setIsLoading(true)
-          await supabase.auth.getUser().then(async (data, err) => {  // 이용자가 관리자인지 확인
-            if (data) {
-              await supabase
+      .then(console.log("this is shifts"));
+  };
+
+  useEffect(() => {
+    // 모든 테이블 정보 한 번에 얻기
+    const fetchTables = async () => {
+      try {
+        setIsLoading(true);
+        await supabase.auth.getUser().then(async (data, err) => {
+          // 이용자가 관리자인지 확인
+          if (data) {
+            await supabase
               .from("admins")
               .select()
               .eq("id", data.data.user.id)
               .then((admin, err) => {
-                if (admin.data.length !== 0) {  // checks if logged in user is registered in the admin table
-                  console.log("this user is an admin")
-                  setUser(admin)
+                if (admin.data.length !== 0) {
+                  // checks if logged in user is registered in the admin table
+                  console.log("this user is an admin");
+                  setUser(admin);
                 } else {
-                  console.log("this user is not an admin")
+                  console.log("this user is not an admin");
                 }
-              })
-            }
-          }) 
-          fetchProfiles()
-          fetchSignups()
-          fetchShifts()
-        } catch (err) {
-          console.log("caught error")
-        } finally {
-          setIsLoading(false)
-        }
+              });
+          }
+        });
+        fetchProfiles();
+        fetchSignups();
+        fetchShifts();
+      } catch (err) {
+        console.log("caught error");
+      } finally {
+        setIsLoading(false);
       }
-      fetchTables()
-    }, [])
+    };
+    fetchTables();
+  }, []);
 
-    const logout = async () => {
-      let success = false
-      await supabase.auth.signOut().then(() => {
-          success = true
-      })
-      if (success) router.push("/")
-    }
+  const logout = async () => {
+    let success = false;
+    await supabase.auth.signOut().then(() => {
+      success = true;
+    });
+    if (success) router.push("/");
+  };
 
-    if (isLoading || !people || !signups || !shifts) {  // 한 가지라도 로드 안 됐으면...
-      return <Loading />  
-    }
+  if (isLoading || !people || !signups || !shifts) {
+    // 한 가지라도 로드 안 됐으면...
+    return <Loading />;
+  }
 
-    if (!user) {  // 이용자가 관리자 않이라서 쫓겨난 경우
-        return <Reroute />  
-    }
-  
-    // NOTE: According to https://stackoverflow.com/questions/66729498/next-js-is-not-rendering-css-in-server-side-rendering
-    // If you are not in production, the CSS/styles will not be loaded on the first fetch (refresh to see)
-    
-    return (
+  if (!user) {
+    // 이용자가 관리자 않이라서 쫓겨난 경우
+    return <Reroute />;
+  }
+
+  // NOTE: According to https://stackoverflow.com/questions/66729498/next-js-is-not-rendering-css-in-server-side-rendering
+  // If you are not in production, the CSS/styles will not be loaded on the first fetch (refresh to see)
+
+  return (
+    <div>
+      {/* make a side navbar for  */}
+      <button onClick={() => logout()}>Logout</button>
+
       <div>
-        <button onClick={() => logout()}>Logout</button>
-
-        <div>
-          <button onClick={() => setContent("Profiles")}>Profiles</button>
-          <button onClick={() => setContent("Signups")}>Signups</button>
-          <button onClick={() => setContent("Shifts")}>Shifts</button>
-          <button onClick={() => setContent("None")}>Hide all tables</button>
-        </div>
-        
-        {/* <p>Content is: {content}</p> */}
- 
-        {content === 'Profiles' && <Profiletable profiles={people} />}
-        {content === 'Signups' && <Signuptable signups={signups} shifts={shifts}/> }
-        {content === 'Shifts' && <Shiftstable signups={signups} shifts={shifts} /> }
-
+        <button onClick={() => setContent("Profiles")}>Profiles</button>
+        <button onClick={() => setContent("Signups")}>Signups</button>
+        <button onClick={() => setContent("Shifts")}>Shifts</button>
+        <button onClick={() => setContent("None")}>Hide all tables</button>
       </div>
-    )
+      {/* <p>Content is: {content}</p> */}
+
+      {content === "Profiles" && <Profiletable profiles={people} />}
+      {content === "Signups" && (
+        <Signuptable signups={signups} shifts={shifts} />
+      )}
+      {content === "Shifts" && (
+        <Shiftstable signups={signups} shifts={shifts} />
+      )}
+    </div>
+  );
 }
--- app/login.jsx
+++ app/login.jsx
@@ -1,74 +1,88 @@
-'use client'
+"use client";
 
-import { useRef } from "react"
-import { useRouter } from "next/navigation"
-import { supabase } from "../supabaseClient"
-import "../styles/login.css"
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../supabaseClient";
+import "../styles/login.css";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const router = useRouter();
 
-    const emailRef = useRef("")
-    const passwordRef = useRef("")
-    const router = useRouter()
+  const login = async (e) => {
+    e.preventDefault();
+    let success = false;
 
-    const login = async (e) => {
-        e.preventDefault()
-        let success = false
+    await supabase.auth
+      .signInWithPassword({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+      .then(({ data, error }) => {
+        if (error) {
+          alert(error.message);
+        } else if (data) {
+          success = true;
+        }
+      });
+    if (success) router.push("/home");
+  };
 
-        await supabase.auth
-            .signInWithPassword({
-                email: emailRef.current.value,
-                password: passwordRef.current.value,
-            })
-            .then(({data, error}) => {
-                if (error) {
-                    alert(error.message)
-                } else if (data) {
-                    success = true
-                }
-            })
-            if (success) router.push("/home")
-    }
+  return (
+    <div className="body">
+      <div className="section">
+        {/* 🤮 못생겨서 취소 🤮  */}
+        {/* <div className="color"></div>
+        <div className="color"></div>
+        <div className="color"></div>
+        <div className="color"></div> */}
 
-    return(
-        <div className="body"> 
-            <div className="section">  
+        <div className="login-box">
+          <div class="login-square"></div>
+          <div class="login-square"></div>
+          <div class="login-square"></div>
+          <div class="login-square"></div>
+          <div class="login-square"></div>
 
-                {/* 🤮 못생겨서 취소 🤮  */}
-                {/* <div className="color"></div> 
-                <div className="color"></div>
-                <div className="color"></div> 
-                <div className="color"></div> */}
-
-                <div className="login-box"> 
-
-                    <div class="login-square"></div>
-                    <div class="login-square"></div>
-                    <div class="login-square"></div>
-                    <div class="login-square"></div>
-                    <div class="login-square"></div>
-
-                    <div className="login-container">
-                        <div className="login-form"> 
-                            <h2>MySmile Admin Portal</h2>
-                            <form className="login-formbox" method="POST" onSubmit={(e) => login(e)}>
-                                <div className="login-inputBox">
-                                    <input ref={emailRef} name="email" type="email" autoComplete="email" placeholder="Email" required></input>
-                                </div>
-                                <div className="login-inputBox">
-                                    <input ref={passwordRef} name="password" type="password" autoComplete="password" placeholder="Password" required></input>
-                                </div>
-                                <div className="login-inputBox submitbtn">
-                                    <input type="submit" value="Login"></input>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div> 
-
+          <div className="login-container">
+            <div className="login-form">
+              <h2>MySmile Admin Portal</h2>
+              <form
+                className="login-formbox"
+                method="POST"
+                onSubmit={(e) => login(e)}
+              >
+                <div className="login-inputBox">
+                  <input
+                    ref={emailRef}
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder="Email"
+                    required
+                  ></input>
+                </div>
+                <div className="login-inputBox">
+                  <input
+                    ref={passwordRef}
+                    name="password"
+                    type="password"
+                    autoComplete="password"
+                    placeholder="Password"
+                    required
+                  ></input>
+                </div>
+                <div className="login-inputBox submitbtn">
+                  <input type="submit" value="Login"></input>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
