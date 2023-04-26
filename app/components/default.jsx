cfbd7decf94c0bd466a4fb974140b69f87239475
han5010 <error404@live.unc.edu>
2023-04-25 22:26:43 -0400

    Add link to docs on app landing page

--- app/components/default.jsx
+++ app/components/default.jsx
@@ -6,7 +6,7 @@ const Defaultpage = () => {
             <div className="default-bg-img">
                 <div className="welcome-msg-container">
                     <h1>Welcome to the Wake Smiles Admin Portal</h1>
-                    <p className="text-[#00539b]">Click one of the tabs to get started</p>
+                    <a href="https://docs.google.com/document/d/1Um27MsmFS_Nmrjn9ebP2f57PuxLern-fNlpt0cVNgQk/edit?usp=sharing" target="_blank" className="text-[#00539b] text-xl">View Docs</a>
                 </div>
             </div>
         </div>
